bce5fdc531ea3dddb502ea3a7a7376bcc38b7a87
ajitaani <164158530+ajitaani@users.noreply.github.com>
2024-03-31 23:43:41 -0700

    Create welcome1.php
    Showing Codeigniter functioning

---
+++ application/views/welcome1.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>Welcome to codedigniter practical MVC</title>
+    </head>
+  <body>
+    <h1>Hello Students welcome to new world</h1>h1>
+    <div id="bodfy">
+      <p> World of Code Igniter View of MVC</p>
+    </div>div>
+  </body>
+</html>
